22ab14892e466187af1fe58daa9aadb2f75bfae7
Serhey1992 <makss92i@gmail.com>
2022-12-11 13:41:31 +0200

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkup = makeGalleruMarkup(galleryItems);
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkup);
+galleryEl.addEventListener("click", onGalleryImageClick);
+
+
+function makeGalleruMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+      <a class="gallery__link href="${original}">
+    <img class="gallery__image" src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    /></a></div>`
+    }).join("");
+}
+
+function onGalleryImageClick(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains("gallery__image")) {
+        return;
+    }
+
+    const modalImg = evt.target.dataset.source;
+    const modal = basicLightbox.create(`<img src="${modalImg}">`);
+
+    modal.show();
+
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+const gallleryMarkup = makeGalleruMarkup(galleryItems);
+galleryEl.insertAdjacentHTML("afterbegin", gallleryMarkup);
 
-console.log(galleryItems);
+
+function makeGalleruMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    }).join("");
+}
+
+const imageList = new SimpleLightbox(".gallery a", {
+  captionData: "alt",
+  captionDelay: 300,
+  captionType: "alt",
+});
